7de284933780361e4ce404a1c08340c0d6f8cbfe
Joo Aun Saw <jasaw@dius.com.au>
2017-10-26 17:02:57 +1100

    read LDR in a loop

--- Makefile
+++ Makefile
@@ -4,8 +4,6 @@ endif
 
 CFLAGS += -I. -I$(SRC) -Wall -std=c99 -D_DEFAULT_SOURCE=1
 
-LIBS += -lrt
-
 all: ldr-reader
 
 ldr-reader: ldr-reader.o sysfsgpio.o utils.o
--- ldr-reader.c
+++ ldr-reader.c
@@ -16,10 +16,8 @@
  */
 
 #include <stdlib.h>
-//#include <fcntl.h>
-//#include <unistd.h>
-//#include <sys/time.h>
-//#include <poll.h>
+#include <time.h>
+#include <signal.h>
 
 #include "utils.h"
 #include "sysfsgpio.h"
@@ -34,6 +32,15 @@ static struct ldr_t ldr =
 {
    .gpio = -1,
 };
+static unsigned char terminate = 0;
+
+
+
+void handle_terminate_signal(int sig)
+{
+   if ((sig == SIGTERM) || (sig == SIGINT))
+      terminate = 1;
+}
 
 
 int main(int argc, char *argv[])
@@ -42,26 +49,51 @@ int main(int argc, char *argv[])
    //struct timeval tv;
    //struct pollfd pfd;
    //int fd;
+   struct timespec start_time;
+   struct timespec now;
    int ret = 0;
    //char buf[8];
 
+   set_log_level(LOG_DEBUG);
+
    // TODO: take GPIO from arg
    ldr.gpio = 17;
-   ret |= gpio_export(ldr.gpio);
-   ret |= gpio_direction(ldr.gpio, GPIO_IN);
-   ret |= gpio_edge(ldr.gpio, GPIO_EDGE_RISING);
 
-   if (ret != 0) {
-      gpio_unexport(ldr.gpio);
-      return ret;
+   signal(SIGINT, handle_terminate_signal);
+   signal(SIGTERM, handle_terminate_signal);
+
+   ret |= gpio_export(ldr.gpio);
+
+
+
+   int time_diff_ms;
+   int poll_ret;
+   while (!terminate) {
+      // drain capacitor
+      //udelay(100000);
+      ret |= gpio_direction(ldr.gpio, GPIO_OUT);
+      //udelay(100000);
+      ret |= gpio_write(ldr.gpio, GPIO_LOW);
+      udelay(100000);
+      // change to input to let capacitor charge
+      ret |= gpio_direction(ldr.gpio, GPIO_IN);
+      ret |= gpio_edge(ldr.gpio, GPIO_EDGE_RISING);
+      if (ret != 0)
+         break;
+
+      clock_gettime(CLOCK_MONOTONIC, &start_time);
+      poll_ret = gpio_wait_for_interrupt(ldr.gpio, 3000);
+      clock_gettime(CLOCK_MONOTONIC, &now);
+      if (poll_ret >= 0) {
+         time_diff_ms = (now.tv_sec - start_time.tv_sec) * 1000 + (now.tv_nsec - start_time.tv_nsec) / 1000000;
+         LOG_VERBOSE("%d ms\n", time_diff_ms);
+      }
+      ret |= gpio_edge(ldr.gpio, GPIO_EDGE_NONE);
    }
 
 
 
-   gpio_wait_for_interrupt(ldr.gpio, 3000);
-
-
-
+clean_up:
    gpio_unexport(ldr.gpio);
 
    exit(0);
